d812cdccb2bde359ac4231a16ecad6b73f63698e
Lukas Böhm <boehm-lukas@outlook.de>
2022-01-07 15:32:50 +0100

    :boom: first working version

--- background.js
+++ background.js
@@ -1,12 +1,21 @@
-function reopenTabs() {
-    browser.tabs.query()
-    
-    // let tabs
-    
+async function reopenTabs() {
+  try {
+    // get all incognito tabs from current window
+    let tabs = await browser.tabs.query({
+      currentWindow: true,
+    });
 
-    // browser.tabs.create({
-    //     url:
-    // })   
+    // create new window
+    await browser.windows.create({
+      focused: true,
+      incognito: false,
+      url: tabs.map((x) => x.url),
+    });
+
+    // TODO close old (private) window?
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 browser.browserAction.onClicked.addListener(reopenTabs);
